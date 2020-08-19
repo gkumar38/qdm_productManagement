40baee00182942350e96a581958bab229c15eab2
gkumar38 <35846630+gkumar38@users.noreply.github.com>
2020-08-19 22:18:35 +0530

    Update ManagedCareServiceListController.java

--- src/main/java/com/qdmProductMangement/Controller/ManagedCareServiceListController.java
+++ src/main/java/com/qdmProductMangement/Controller/ManagedCareServiceListController.java
@@ -7,10 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.qdmProductManagement.Service.ManagedCareService;
 import com.qdmProductMangement.Entity.ServiceProvided;
 import com.qdmProductMangement.Entity.ServicesData;
+import com.qdmProductMangement.Service.ManagedCareService;
 
 @RestController
 @RequestMapping("/careSupervisor/management")
